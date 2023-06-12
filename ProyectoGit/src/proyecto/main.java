5d89d5117a0de7fbb3c555271ebfea1b33da0f64
Miguel F.A. <mfigalo996@educaand.es>
2023-06-12 04:11:05 +0200

    Suma

---
+++ ProyectoGit/src/proyecto/main.java
@@ -0,0 +1,28 @@
+package proyecto;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int numero1, numero2, resultado;
+		System.out.print("Introduce primer número: ");
+		numero1 = sc.nextInt();
+		System.out.print("Introduce segundo número: ");
+		numero2 = sc.nextInt();
+		
+		resultado = suma(numero1,numero2);
+		
+		System.out.println("El resultado es: " + resultado);
+		
+	}
+
+	public static int suma(int a, int b) {
+		int c;
+		c = a + b;
+		return c;
+		}
+
+}
