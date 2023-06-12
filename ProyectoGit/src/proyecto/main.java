06bc2e67db977d46f486b2e06772724f677a6449
Miguel F.A. <mfigalo996@educaand.es>
2023-06-12 04:47:24 +0200

    Producto

--- ProyectoGit/src/proyecto/main.java
+++ ProyectoGit/src/proyecto/main.java
@@ -7,7 +7,7 @@ public class main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int numero1, numero2, resultado, resultado2;
+		int numero1, numero2, resultado, resultado2, resultado3, resultado4;
 		System.out.print("Introduce primer número: ");
 		numero1 = sc.nextInt();
 		System.out.print("Introduce segundo número: ");
@@ -15,9 +15,13 @@ public class main {
 		
 		resultado = suma(numero1,numero2);
 		resultado2 = resta(numero1,numero2);
+		resultado3 = producto(numero1,numero2);
+		//resultado4 = resta(numero1,numero2);
 		
 		System.out.println("El resultado es: " + resultado);
 		System.out.println("El resultado es: " + resultado2);
+		System.out.println("El resultado es: " + resultado3);
+		//System.out.println("El resultado es: " + resultado4);
 		
 	}
 	
@@ -28,6 +32,12 @@ public class main {
 	}
 	
 	public static int resta(int a, int b) {
+		int c;
+		c = a + b;
+		return c;
+	}
+	
+	public static int producto(int a, int b) {
 		int c;
 		c = a + b;
 		return c;
