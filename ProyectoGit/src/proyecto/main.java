308cfdfc643cad41ae61a8e1c9356b7c8bb798ba
Miguel F.A. <mfigalo996@educaand.es>
2023-06-12 04:39:44 +0200

    Suma

--- ProyectoGit/src/proyecto/main.java
+++ ProyectoGit/src/proyecto/main.java
@@ -7,29 +7,21 @@ public class main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int numero1, numero2, resultado, resultado2;
+		int numero1, numero2, resultado;
 		System.out.print("Introduce primer número: ");
 		numero1 = sc.nextInt();
 		System.out.print("Introduce segundo número: ");
 		numero2 = sc.nextInt();
 		
 		resultado = suma(numero1,numero2);
-		resultado2 = resta(numero1,numero2);
 		
-		System.out.println("El resultado de la suma es: " + resultado
-				+ "\nEl resultado de la resta es: " + resultado2);
+		System.out.println("El resultado es: " + resultado);
 		
 	}
 
 	public static int suma(int a, int b) {
 		int c;
 		c = a + b;
-		return c;
-	}
-	
-	public static int resta(int a, int b) {
-		int c;
-		c = a - b;
 		return c;
 	}
 
